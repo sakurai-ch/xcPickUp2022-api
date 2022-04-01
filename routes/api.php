c897b321c1e80cae0ed9aec7c39069601850fa26
sakurai <sakurai.ch@gmail.com>
2022-04-01 21:02:15 +0900

    fix: comment

--- routes/api.php
+++ routes/api.php
@@ -5,17 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlayersController;
 use App\Http\Controllers\DriversController;
 
-// Route::group(
-//     [
-//         'prefix' => 'v1'
-//     ],
-//     function ($router) {
-        Route::get('/v1/players', [PlayersController::class, 'getPlayers']);
-        Route::put('/v1/players', [PlayersController::class, 'putPlayer']);
-        Route::get('/v1/drivers', [DriversController::class, 'getDrivers']);
-        Route::post('/v1/drivers', [DriversController::class, 'postDriver']);
-//     }
-// );
+Route::get('/v1/players', [PlayersController::class, 'getPlayers']);
+Route::put('/v1/players', [PlayersController::class, 'putPlayer']);
+Route::get('/v1/drivers', [DriversController::class, 'getDrivers']);
+Route::post('/v1/drivers', [DriversController::class, 'postDriver']);
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
