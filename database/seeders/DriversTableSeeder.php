877687f8cd187b2076c17cf498a6c359d60df044
sakurai <sakurai.ch@gmail.com>
2022-04-12 22:36:30 +0900

    change: seeding data

--- database/seeders/DriversTableSeeder.php
+++ database/seeders/DriversTableSeeder.php
@@ -10,18 +10,27 @@ class DriversTableSeeder extends Seeder
 {
     public function run()
     {
-        // $params =
-        //     [
-        //         ['name' => 'sample', 'capacity' => 0],
-        //     ];
+        $params =
+            [
+                ['name' => '池野', 'capacity' => 4],
+                ['name' => 'もとこ', 'capacity' => 4],
+                ['name' => '木内', 'capacity' => 4],
+                ['name' => '倉井', 'capacity' => 4],
+                ['name' => '倉上', 'capacity' => 4],
+                ['name' => '斎藤和美', 'capacity' => 4],
+                ['name' => '斉まき', 'capacity' => 4],
+                ['name' => '園部', 'capacity' => 4],
+                ['name' => '田中', 'capacity' => 4],
+                ['name' => 'ダニエル', 'capacity' => 4],
+                ['name' => '徳永', 'capacity' => 4],
+            ];
 
-        // $no = 0;
-        // $now = Carbon::now();
-        // foreach ($params as $param) {
-        //     $param['created_at'] = $now;
-        //     $param['updated_at'] = $now;
+        $now = Carbon::now();
+        foreach ($params as $param) {
+            $param['created_at'] = $now;
+            $param['updated_at'] = $now;
             
-        //     Driver::insert($param);
-        // }
+            Driver::insert($param);
+        }
     }
 }
