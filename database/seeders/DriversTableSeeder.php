06706ac163df7291d326660bad8198ff54efab7b
sakurai <sakurai_ch@yahoo.co.jp>
2024-03-30 21:46:20 +0900

    fix: driver name

--- database/seeders/DriversTableSeeder.php
+++ database/seeders/DriversTableSeeder.php
@@ -12,14 +12,16 @@ class DriversTableSeeder extends Seeder
     {
         $params =
             [
-                ['name' => '清水', 'capacity' => 4],
-                ['name' => '小山', 'capacity' => 8],
-                ['name' => '大塚', 'capacity' => 8],
-                ['name' => '中里', 'capacity' => 8],
-                ['name' => '杉山', 'capacity' => 8],
-                ['name' => '堀', 'capacity' => 26],
+                ['name' => '清水4（4人）', 'capacity' => 4],
+                ['name' => '小山1（8人）', 'capacity' => 8],
+                ['name' => '大塚2（8人）', 'capacity' => 8],
+                ['name' => '中里5（8人）', 'capacity' => 8],
+                ['name' => '杉山3（8人）', 'capacity' => 8],
+                ['name' => '堀（26人）', 'capacity' => 26],
                 ['name' => 'GL', 'capacity' => 70],
                 ['name' => 'ML', 'capacity' => 70],
+                ['name' => 'ABS', 'capacity' => 70],
+                ['name' => '帰着', 'capacity' => 70],
             ];
 
         $now = Carbon::now();
